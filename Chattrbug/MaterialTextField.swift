e86c2af827a7796343baec39129cdb014f3dd719
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-04-27 21:41:47 -0400

    finished UI for login screen

---
+++ Chattrbug/MaterialTextField.swift
@@ -0,0 +1,28 @@
+//
+//  MaterialTextField.swift
+//  Chattrbug
+//
+//  Created by Gabriele on 4/27/16.
+//  Copyright © 2016 Ashley Donohoe. All rights reserved.
+//
+
+import UIKit
+
+class MaterialTextField: UITextField {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = 2.0
+        layer.borderColor = UIColor(red: SHADOW_COLOR, green: SHADOW_COLOR, blue: SHADOW_COLOR, alpha: 0.1).CGColor
+        layer.borderWidth = 1.0
+    }
+    
+    // For placeholder
+    override func textRectForBounds(bounds: CGRect) -> CGRect {
+        return CGRectInset(bounds, 10, 0)
+    }
+    
+    override func editingRectForBounds(bounds: CGRect) -> CGRect {
+        return CGRectInset(bounds, 10, 0)
+    }
+
+}

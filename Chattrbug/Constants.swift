8b3d656c4b065242d8fdadf76e6c859ebfefcae3
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-04-27 20:43:25 -0400

    added custom view and began design of login screen

---
+++ Chattrbug/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Chattrbug
+//
+//  Created by Gabriele on 4/27/16.
+//  Copyright © 2016 Ashley Donohoe. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0/255.0
